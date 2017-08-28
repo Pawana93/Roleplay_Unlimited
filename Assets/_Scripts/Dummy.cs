7b151c0f0aa503ae8bdbd879bdc2bca7bc5bf689
Pawana93 <pawana.schirkeinst@googlemail.com>
2017-08-28 18:12:42 +0200

    Upload of Data
    Uploading every data missing for Unity

---
+++ Assets/_Scripts/Dummy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dummy : MonoBehaviour {
+
+    public int maxHP;
+
+    private int currentHP;
+
+	// Use this for initialization
+	void Start () {
+        currentHP = maxHP;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/_Scripts/PlayerStats.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour {
+
+    public int currentHP;
+    public int maxHP;
+    public int currentMP;
+    public int maxMP;
+    public int level=1;
+    public int exp;
+    public int strength;
+    public int stamina;
+    public int intellect;
+
+    public Slider healthSlider;
+    public Slider manaSlider;
+    public Slider expSlider;
+    public Text txtCurHP;
+    public Text txtMaxHP;
+    public Text txtMaxMP;
+    public Text txtCurMP;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        strength = 30;
+        stamina = 50;
+        intellect = 20;
+        maxHP = level + (level * stamina * 2);
+        currentHP = maxHP;
+        maxMP = level + (level * intellect * 1);
+        currentMP = maxMP;
+        exp = 0;
+        healthSlider.maxValue = maxHP;
+        manaSlider.maxValue = maxMP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthSlider.value = currentHP;
+        manaSlider.value = currentMP;
+        txtMaxHP.text = maxHP.ToString();
+        txtMaxMP.text = maxMP.ToString();
+        txtCurHP.text = currentHP.ToString();
+        txtCurMP.text = currentMP.ToString();
+	}
+}
