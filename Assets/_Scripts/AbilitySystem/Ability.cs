aba29cec2315ef9968c6a1e73cab7c219325e87f
Pawana93 <pawana.schirkeinst@googlemail.com>
2017-09-01 20:39:28 +0200

    Creating Base Ability Script
    creating a base Ability Script

---
+++ Assets/_Scripts/AbilitySystem/Ability.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability {
+
+	private string name;
+	private string description;
+	private Sprite icon;
+	private List<AbilityBehaviors> behaviors;
+	private bool requiresTarget;
+	private bool canCastOnSelf;
+	private int cooldown;  //in seconds
+	private GameObject particleEffect; //needs assigned when creating the ability
+
+	public Ability(string aname, Sprite aicon, List<AbilityBehaviors> abehaviors)
+	{
+		name = aname;
+		icon = aicon;
+		behaviors = new List<AbilityBehaviors> ();
+		behaviors = abehaviors;
+		cooldown = 0;
+		requiresTarget = false;
+		canCastOnSelf = false;
+		description = "Default";
+	}
+
+	public Ability(string aname, string adescription, Sprite aicon, List<AbilityBehaviors> abehaviors, bool arequiretarget, int acooldown, GameObject aparticleeffect)
+	{
+		name = aname;
+		icon = aicon;
+		behaviors = new List<AbilityBehaviors> ();
+		behaviors = abehaviors;
+		cooldown = acooldown;
+		requiresTarget = arequiretarget;
+		canCastOnSelf = false;
+		description = adescription;
+		particleEffect = aparticleeffect;
+	}
+
+	public string AbilityName
+	{
+		get { return name; }
+	}
+
+	public string AbilityDescription
+	{
+		get { return description; }
+	}
+
+	public Sprite AbilityIcon
+	{
+		get { return icon; }
+	}
+
+	public int AbilityCooldown
+	{
+		get { return cooldown; }
+	}
+
+	public List<AbilityBehaviors> AbilityBehaviors
+	{
+		get { return behaviors; }
+	}
+
+	public void UseAbility()
+	{
+		
+	}
+}
--- Assets/_Scripts/AbilitySystem/AbilityBehaviors.cs
+++ Assets/_Scripts/AbilitySystem/AbilityBehaviors.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ability : ScriptableObject {
+public class AbilityBehaviors : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
